952bab12c8cf0c71423c730ee26902a542373e8e
Králik Norbert <kraaliknorbert@gmail.com>
2021-04-12 17:33:34 +0200

    delete console.log

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const pageLoaded = () => {
 
-  console.log("betöltés");
-
   const hamburger = document.querySelector('.hamburger')
   const links = document.querySelector('.nav-items')
 
   hamburger.addEventListener("click", () => {
-    console.log("click");
     links.classList.toggle('active')
   })
 
